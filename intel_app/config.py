c85561064c4597a77bdf361e46cdc2503d7a300b
srinivas-surya <srinivasurya42@gmail.com>
2023-09-30 22:09:41 +0530

    modified : adding backend changes for login functions

---
+++ intel_app/config.py
@@ -0,0 +1,1 @@
+DEFAULT_PASSWORDS = ['Admin#123']
--- intel_app/views.py
+++ intel_app/views.py
@@ -3,27 +3,60 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
-# Create your views here.
+
+from .config import DEFAULT_PASSWORDS
+
 
 @csrf_exempt
-def login(request):
+def user_login(request):
+    context = {}
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        return HttpResponseRedirect('index')
+        # Validating credentials with the database
+        user = authenticate(request, username=username, password=password)
+        if user:
+            if password in DEFAULT_PASSWORDS:
+                '''
+                    if user password matches with default passwords
+                    render the user to password management page
+                '''
+                return HttpResponseRedirect(reverse("home"))
+
+            if list(user.groups.all()):
+                '''
+                    if user not tagged to the any project render error message to UI
+                    else render to the respective home page 
+                '''
+                project = user.groups.all()[0].name
+            else:
+                context['error'] = f"Please tag the project : {username}"
+                return render(request, "intel_app/login.html", context)
+
+            # redirecting to home screen
+            login(request, user)
+            request.session['meta_data'] = {'user_id': username, 'project': project}
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            context['error'] = "provide valid credentials"
+            return render(request, "intel_app/login.html", context)
     else:
         return render(request, "intel_app/login.html")
+
 
 def user_logout(request):
     logout(request)
     try:
-        del request.session["user_group"]
-    except:
+        del request.session['meta_data']
+    except KeyError:
         pass
     return HttpResponseRedirect(reverse('login'))
+
 
 def forgot_password(request):
     return render(request, 'intel_app/forgot_password.html')
 
-def index(request):
+
+def home(request):
+    print(request.session['meta_data'])
     return render(request, 'intel_app/index.html')
