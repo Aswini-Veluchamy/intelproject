73a571b54f86998a483d7b747a6ff0504002b618
Aswini Veluchamy <45841530+Aswini-Veluchamy@users.noreply.github.com>
2023-09-30 17:29:19 +0530

    modified

--- intel_app/urls.py
+++ intel_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.login, name='login'),
+    path('logout/', views.user_logout, name="logout"),
+    path('forgot_password/', views.forgot_password, name='forgot_password'),
+    path('index/', views.index, name='index'),
 ]
--- intel_app/views.py
+++ intel_app/views.py
@@ -1,6 +1,29 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
+@csrf_exempt
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        return HttpResponseRedirect('index')
+    else:
+        return render(request, "intel_app/login.html")
+
+def user_logout(request):
+    logout(request)
+    try:
+        del request.session["user_group"]
+    except:
+        pass
+    return HttpResponseRedirect(reverse('login'))
+
+def forgot_password(request):
+    return render(request, 'intel_app/forgot_password.html')
+
 def index(request):
-    return render(request, 'intel_app/index2.html')
+    return render(request, 'intel_app/index.html')
