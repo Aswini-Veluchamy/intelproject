1ef1e67f6a9e30401994d3922db873a93ef79c64
srinivas-surya <srinivasurya42@gmail.com>
2023-10-09 22:00:54 +0530

    modified: added quill config

--- intel_app/models.py
+++ intel_app/models.py
@@ -10,20 +10,24 @@ class KeyMessage(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.message
+        return self.created_at
 
     class Meta:
         db_table = "key_message"
 
+
 class KeyMessageTable(models.Model):
     ''' created database fields'''
     message = models.CharField(max_length=200)
+    user = models.CharField(max_length=200)
+    project = models.CharField(max_length=200)
 
     def __str__(self):
         return self.message
 
     class Meta:
         db_table = "key_message_table"
+
 
 class RiskTable(models.Model):
     ''' created database fields'''
@@ -32,8 +36,9 @@ class RiskTable(models.Model):
     owner = models.CharField(max_length=200)
     comments = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
+
     def __str__(self):
-        return self.message
+        return self.owner
 
     class Meta:
         db_table = "risk_table"
